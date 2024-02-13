89847e632e75daebf04092f26a4af809933afd20
Andres Tellez <andresfelipetellez1998@gmail.com>
2024-02-13 11:03:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ window.onscroll = function(){
 
 function downloadCV() {
     // Ruta completa del archivo PDF
-    var url = 'https://local/pdf/cv.pdf';
+    var url = 'https://pdf/cv.pdf';
 
     // Crear un elemento de enlace temporal
     var link = document.createElement('a');
